207416ccad63ce01a72b1c63e79dd782a9734c3b
adrie <adrie@DESKTOP-KOMI5BU>
2017-03-17 15:01:00 +0100

    # WARNING: head commit changed in the meantime
    Add BDD correct file

---
+++ server/BDD/chatirc.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Ven 17 Mars 2017 à 13:17
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `chatirc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `channel`
+--
+
+CREATE TABLE `channel` (
+  `channel_id` int(11) NOT NULL,
+  `channel_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `post`
+--
+
+CREATE TABLE `post` (
+  `post_id` int(11) NOT NULL,
+  `content` varchar(20000) NOT NULL,
+  `post_date` date NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `channel_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `User_id` int(11) NOT NULL,
+  `User_name` varchar(255) CHARACTER SET latin1 NOT NULL,
+  `Connection_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `channel`
+--
+ALTER TABLE `channel`
+  ADD PRIMARY KEY (`channel_id`);
+
+--
+-- Index pour la table `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`post_id`),
+  ADD KEY `channel_id` (`channel_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`User_id`);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `post`
+--
+ALTER TABLE `post`
+  ADD CONSTRAINT `foreignkey_channel` FOREIGN KEY (`channel_id`) REFERENCES `channel` (`channel_id`),
+  ADD CONSTRAINT `foreignkey_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`User_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
