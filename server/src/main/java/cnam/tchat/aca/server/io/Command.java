e66ecae95269812086d67fd3a1d74f1d459e31d6
Gabardin <c.davulcu1995@gmail.com>
2017-04-10 23:15:42 +0200

    Command manager

---
+++ server/src/main/java/cnam/tchat/aca/server/io/Command.java
@@ -0,0 +1,16 @@
+package cnam.tchat.aca.server.io;
+
+public class Command {
+
+	public static void Commande(String order, String arguments){
+		if(order == "connect"){
+			// établir la connexion
+		}else if(order == "kik"){
+			// bannir la personne
+		}else if(order == "channel"){
+			// joindre la channel 
+		}else if(order == "combien"){
+			// nous sommes x personnes connectés
+		}
+	}
+}
